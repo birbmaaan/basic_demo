482fdf13071a8c629693d57d168b622e6d738231
birdmaaan <elijah.b.dove@gmail.com>
2023-10-26 14:12:30 -0700

    fix identify

--- src/App.js
+++ src/App.js
@@ -12,6 +12,21 @@ like Appcues.on so that they will be called every time the page loads.
 */
 
 const App = () => {
+
+  if (true) {
+    window.AppcuesSettings = { enableURLDetection: true};
+    const user = window.user;
+    const appcuesScript = document.createElement('script');
+    appcuesScript.src = 'https://fast.appcues.com/85728.js';
+
+    appcuesScript.onload = () => 
+      console.log('testing identify');
+      // window.Appcues.identify(user);
+    
+    const body = document.getElementById('body');
+    body.append(appcuesScript);
+  }
+
   if (!window.localStorage.currentUser) { // persists the current user across page refresh
     window.localStorage.setItem("currentUser", "testUser"); // if no current user, set userID to "testUser"
   }
@@ -23,31 +38,35 @@ const App = () => {
   const userId = window.localStorage.currentUser;
   const currentTime = parseInt(window.localStorage.timestamp);
 
-  window.Appcues.identify(userId, { // feel free to add in any user properties here
-    role: "tester",
-    signupDate: currentTime,
-  })
+  console.log('hello');
+  // window.Appcues.identify(543211, { // feel free to add in any user properties here
+  //   school: {
+  //     id: 12345
+  //   },
+  //   role: "tester",
+  //   signupDate: currentTime,
+  //   email: 'test@test.com'
+  // })
+//  window.Appcues.page()
 
   // setTimeout(() => {
   //   console.log("Delayed for 1 second.");
   //   window.Appcues.group('group1', {group1: "true"});
   // }, "1000")
     
-
+// window.Appcues.loadLaunchpad('#launchpad', { position: 'right' });
 
 
   // window.Appcues.anonymous();
 
-  window.Appcues.on("all", function(name, payload) {
-    // console.log(name);     // uncomment these two lines if you'd like to
-    // console.log(payload);  // log all events into the dev tools console
+  // window.Appcues.on("all", function(name, payload) {
+  //   // console.log(name);     // uncomment these two lines if you'd like to
+  //   // console.log(payload);  // log all events into the dev tools console
 
-    logEvents(name, payload); // adds events into the event log on the page
-  })
+  //   logEvents(name, payload); // adds events into the event log on the page
+  // })
 
-  window.Appcues.loadLaunchpad("#launchpad", { // loads Launchpad on this element
-    position: "right"
-  })
+  console.log('all done');
   
   return ( 
     <BrowserRouter> 
