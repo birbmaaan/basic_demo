1c9170f74ef2ad7d3afe66030faba9c6fb91022a
birdmaaan <elijah.b.dove@gmail.com>
2023-10-27 16:16:08 -0700

    fix dependencies

--- src/Appcues.js
+++ src/Appcues.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Route, Switch, useLocation, useHistory } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from "./Home";
 import About from "./About";
